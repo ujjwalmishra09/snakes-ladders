ebd898300d1218fab71b1d486a41b333fcd9c54a
Ujjwal Mishra <ujjwalmishra09@gmail.com>
2024-02-13 19:32:29 +0530

    add all code

---
+++ components/GameStatus.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="game-status">
+        <Dice />
+        <button @click="gameStore.rollDice"> {{ !gameStore.state.started ? 'Start' : 'Roll Dice' }}</button>
+        <span>Player 1</span>
+        <Coin :style="{position: 'static', display: 'inline-block' }"/>
+    </div>
+
+</template>
+<script setup lang="ts">
+const gameStore = game()
+</script>
+<style>
+.game-status {
+    margin-top: 10px;
+    border: dotted;
+    border-color: black;
+    display: flex;
+    background: rgba(0, 128, 0, 0.3);
+    width: 450px;
+    align-items: center;
+    justify-content: center;
+}
+
+.game-status > div {
+  margin: 5px;
+  text-align: center;
+}
+
+.game-status > button {
+  margin: 5px;
+  text-align: center;
+}
+</style>
